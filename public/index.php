a01f2296c24be5b8bb12531fb0642ee2ad40b923
leetaehee <lastride25@naver.com>
2020-04-25 18:33:52 +0900

    php 디버깅 테스트

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
 	// 지금시각
 	$nowTime = date('Y-m-d H:i:s');
+
+	// pdo
+    $db = new PDO('mysql:host=localhost;dbname=myapp','homestead', 'secretKK1234!@');
+    //$db = new PDO('oci:host=localhost;dbname=TEST','test', 'testdb1234');
 
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/../template/index.html.php';
