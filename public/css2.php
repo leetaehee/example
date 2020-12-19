247187660955ea1516a9b51554531aaedd654fe0
leetaehee <lastride25@naver.com>
2020-12-19 14:06:29 +0900

    브릿지 패턴 namespace, use 키워드 사용

---
+++ public/css2.php
@@ -0,0 +1,2 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../template/css2.html.php';
---
+++ template/css2.html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>코딩의 민족</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1 id="title">코딩의 민족</h1>
+    </body>
+</html>
