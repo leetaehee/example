2a806386273e9e5cc800562eefae6a94045b5514
leetaehee <lastride25@naver.com>
2021-01-03 08:46:11 +0900

    phpstorm debug.php 에서 debug...

--- public/debug.php
+++ public/debug.php
@@ -5,11 +5,19 @@ echo "a= " . $a ."<br>";
 $v = add(-10, 1);
 echo "v= " . $v ."<br>";
 
-function add($a, $b)
+$g = add(1000, 50000);
+echo "g= " . display($g) ."<br>";
+
+function add(int $a, int $b) : int
 {
     if ($a < 1) {
         $a = 1;
     }
 
     return $a + $b;
+}
+
+function display(int $value) : string
+{
+    return number_format($value);
 }
