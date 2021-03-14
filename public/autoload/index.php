5048b6ae883e095fe56ede5b89b75f8554ac0b3c
leetaehee <lastride25@naver.com>
2021-03-14 11:05:00 +0900

    autoload 적용

--- public/autoload/index.php
+++ public/autoload/index.php
@@ -12,4 +12,4 @@ $shop->setPrice(1000);
 echo "이 상품의 가격은 " . $shop->getPrice() . "입니다. \n";
 
 $member->setId('lastride25');
-echo "회원의 아이디는 " . $member->getId() . "입니다. \n";
+echo "회원의 아이디는 {$member->getId()} 입니다. \n";
