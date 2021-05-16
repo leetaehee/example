762a3b0f75718166aa7dcc0e488011b483e722ee
leetaehee <lastride25@naver.com>
2021-05-16 09:33:55 +0900

    로그 폴더 추가 gitignore 추가

--- public/monolog.php
+++ public/monolog.php
@@ -3,8 +3,7 @@ require 'vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
 
 $logger = new Logger('[MonoLog]');
-$logger->pushHandler(new StreamHandler('/Users/developerleetaehee/lbems/test.log', Logger::DEBUG));
+$logger->pushHandler(new StreamHandler('../logs//test.log', Logger::DEBUG));
 $logger->info('monolog로 테스트합니다.. 앞으로 개발할 때는 이거로하..', ['log' => 'monolog']);
