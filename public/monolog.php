5345d025e17c4f9d4b26637137cd89fd1fd9175a
leetaehee <lastride25@naver.com>
2021-05-17 09:20:22 +0900

    logs 파일 이동

--- public/monolog.php
+++ public/monolog.php
@@ -5,5 +5,5 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $logger = new Logger('[MonoLog]');
-$logger->pushHandler(new StreamHandler('../logs//test.log', Logger::DEBUG));
+$logger->pushHandler(new StreamHandler('./logs/test.log', Logger::DEBUG));
 $logger->info('monolog로 테스트합니다.. 앞으로 개발할 때는 이거로하..', ['log' => 'monolog']);
