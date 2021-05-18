b3da520d37a79d019c06ecf012f82e4d2162678a
leetaehee <lastride25@naver.com>
2021-05-18 23:27:26 +0900

    monolog 일별 추가

--- public/monolog.php
+++ public/monolog.php
@@ -8,4 +8,4 @@ $logFile = 'monolog_' . date('Ymd') . ".log";
 
 $logger = new Logger('[MonoLog]');
 $logger->pushHandler(new StreamHandler("./logs/{$logFile}", Logger::DEBUG));
-$logger->info('monolog로 테스트합니다.. 앞으로 개발할 때는 이거로하..', ['log' => 'monolog']);
+$logger->info('monolog 테스트', ['log' => 'monolog']);
