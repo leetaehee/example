70e1decfef93abbf30f5ebb36a795c1b55e67d6d
leetaehee <lastride25@naver.com>
2021-05-18 23:26:57 +0900

    monolog 일별 추가

--- public/monolog.php
+++ public/monolog.php
@@ -4,6 +4,8 @@ require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+$logFile = 'monolog_' . date('Ymd') . ".log";
+
 $logger = new Logger('[MonoLog]');
-$logger->pushHandler(new StreamHandler('./logs/test.log', Logger::DEBUG));
+$logger->pushHandler(new StreamHandler("./logs/{$logFile}", Logger::DEBUG));
 $logger->info('monolog로 테스트합니다.. 앞으로 개발할 때는 이거로하..', ['log' => 'monolog']);
