736ec1e788523565d6f2554fb9aad4535ce2750c
leetaehee <lastride25@naver.com>
2021-05-19 16:45:46 +0900

    aa

--- public/monolog.php
+++ public/monolog.php
@@ -7,7 +7,6 @@ use Monolog\Handler\StreamHandler;
 $jsons = json_decode(file_get_contents('./env.json'), true);
 
 $fileName = 'monolog.log.'. date('Ymd') ;
-
 $logger = new Logger('[MonoLog]');
 $logger->pushHandler(new StreamHandler("{$jsons['log_path']}/{$fileName}", Logger::DEBUG));
 $logger->info('monolog 테스트', ['log' => 'monolog']);
