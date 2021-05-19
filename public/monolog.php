91e95f8a7be946c115f67f37df122d1f27b0df09
leetaehee <lastride25@naver.com>
2021-05-19 15:24:14 +0900

    aa

--- public/monolog.php
+++ public/monolog.php
@@ -11,3 +11,4 @@ $fileName = 'monolog.log.'. date('Ymd') ;
 $logger = new Logger('[MonoLog]');
 $logger->pushHandler(new StreamHandler("{$jsons['log_path']}/{$fileName}", Logger::DEBUG));
 $logger->info('monolog 테스트', ['log' => 'monolog']);
+$logger->warning('테스트중..');
