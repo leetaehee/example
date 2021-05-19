96ff4b37a6faf20c0e77a2af54b9d71b6804166f
leetaehee <lastride25@naver.com>
2021-05-19 15:17:54 +0900

    log 구조 개선 env.json 추가

--- public/monolog.php
+++ public/monolog.php
@@ -4,8 +4,10 @@ require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$logFile = 'monolog.log.'. date('Ymd') ;
+$jsons = json_decode(file_get_contents('./env.json'), true);
+
+$fileName = 'monolog.log.'. date('Ymd') ;
 
 $logger = new Logger('[MonoLog]');
-$logger->pushHandler(new StreamHandler("/logs/monolog/{$logFile}", Logger::DEBUG));
+$logger->pushHandler(new StreamHandler("{$jsons['log_path']}/{$fileName}", Logger::DEBUG));
 $logger->info('monolog 테스트', ['log' => 'monolog']);
