b884860683d9d85e53e02299f159f78f57cd0acb
leetaehee <lastride25@naver.com>
2021-05-19 10:29:41 +0900

    log 구조개선

--- public/monolog.php
+++ public/monolog.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$logFile = 'monolog_' . date('Ymd') . ".log";
+$logFile = 'monolog.log.'. date('Ymd') ;
 
 $logger = new Logger('[MonoLog]');
-$logger->pushHandler(new StreamHandler("./logs/{$logFile}", Logger::DEBUG));
+$logger->pushHandler(new StreamHandler("/logs/{$logFile}", Logger::DEBUG));
 $logger->info('monolog 테스트', ['log' => 'monolog']);
