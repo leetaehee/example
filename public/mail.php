e651f99f6e84511df325492d773d2e9c9a3202ad
leetaehee <lastride25@naver.com>
2021-05-25 21:51:27 +0900

    php mailer 테스트

---
+++ public/mail.php
@@ -0,0 +1,20 @@
+<?php
+include_once('./PHPMailer/PHPMailerAutoload.php');
+
+$driver = new PHPMailer();
+
+$driver->IsSMTP();
+$driver->CharSet = 'UTF-8';
+$driver->SMTPSecure = 'ssl';
+$driver->Host = 'smtp.gmail.com';
+$driver->Port = '465';
+$driver->Username = 'developerkimtakgu@gmail.com';
+$driver->Password = 'eqndljnotjklffvs';
+$driver->SMTPAuth = true;
+$driver->From = 'developerkimtakgu@gmail.com';
+$driver->FromName = 'example';
+$driver->Subject = 'PHP Mailer Test..';
+$driver->msgHTML('PHP Mailer Test..');
+$driver->addAddress('lastride25@naver.com');
+
+$driver->send();
