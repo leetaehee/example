fe89944a5829fa6d0c8f54787b8a7b5eef40a164
leetaehee <lastride25@naver.com>
2021-06-07 22:15:34 +0900

    xss

--- template/xss_test.html.php
+++ template/xss_test.html.php
@@ -6,8 +6,9 @@
     </head>
     <body>
         Hi. <br>
+        html 데이터 출력하기- <?=$value?> <br>
         <form action="<?php echo $_SERVER['PHP_SELF'];?>">
-            <input type="text"  name="memo" value="<?php echo $value; ?>">
+            <input type="text"  name="memo" value="">
             <input type="submit" value="전송하기">
         </form>
     </body>
