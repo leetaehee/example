5b8bce739448a0526875dcd71d7552614c3a8af5
leetaehee <lastride25@naver.com>
2021-07-04 22:45:39 +0900

    recursive.php 추가

--- public/recursive.php
+++ public/recursive.php
@@ -4,6 +4,14 @@
  */
 class Calculate
 {
+    /**
+     * 더하기.
+     *
+     * @param int $a
+     * @param bool $recursive
+     *
+     * @return int
+     */
     public function add(int $a, bool $recursive = false) : int
     {
         $result = 0;
@@ -23,10 +31,12 @@ class Calculate
     }
 }
 
+
 $calculate = new Calculate();
 
+// 유닛테스트
 $a = 5;
-
 $result = $calculate->add($a);
 
+// 출력
 echo "result = {$result} \n";
